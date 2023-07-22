1f306704620cf0c605eb8a18e08afa229647f5a8
xhev-dev <devxhev@gmail.com>
2023-07-23 01:16:53 +0200

    login finished

--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -1,4 +1,5 @@
-import { NavigationProp, ParamListBase } from '@react-navigation/native';
+import CheckBox from '@react-native-community/checkbox';
+import {NavigationProp, ParamListBase} from '@react-navigation/native';
 import React, {useState} from 'react';
 import {
   Image,
@@ -9,6 +10,7 @@ import {
   SafeAreaView,
   TextInput,
 } from 'react-native';
+import { Button } from 'react-native-paper';
 // import styles from './Login.styles';
 // import CheckBox from '@react-native-community/checkbox';
 // import { Button } from 'react-native-paper';
@@ -16,7 +18,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 // const myIcon = ;
 
 interface Props {
-  navigation: NavigationProp<ParamListBase>
+  navigation: NavigationProp<ParamListBase>;
 }
 
 function Login(props: Props): JSX.Element {
@@ -31,48 +33,76 @@ function Login(props: Props): JSX.Element {
             source={require('../../assets/logo.png')}
             style={styles.logo}
           />
-          <View>
+          <View style={styles.content}>
             <Text style={styles.title}>Login to your account</Text>
 
             <View style={styles.loginFormContainer}>
-              <TextInput
-                placeholder="Username / Email"
-                placeholderTextColor="rgba(35, 31, 32, 0.5)"
-                style={styles.input}
-              />
-              <TextInput
-                placeholder="Password"
-                placeholderTextColor="rgba(35, 31, 32, 0.5)"
-                style={styles.input}
-              />
-              <Icon name="rocket" size={30} color="#900" />
-            </View> 
+              <View style={styles.inputContainer}>
+                <TextInput
+                  placeholder="Username / Email"
+                  placeholderTextColor="rgba(35, 31, 32, 0.5)"
+                  style={styles.input}
+                />
+                <Icon
+                  name="user"
+                  size={30}
+                  color="rgba(30, 52, 69, 0.1)"
+                  style={styles.icon}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  placeholder="Password"
+                  placeholderTextColor="rgba(35, 31, 32, 0.5)"
+                  style={styles.input}
+                  secureTextEntry
+                />
+                <Icon
+                  name="key"
+                  size={30}
+                  color="rgba(30, 52, 69, 0.1)"
+                  style={styles.icon}
+                />
+              </View>
+            </View>
+            <View style={styles.otherOptions}>
+              <View style={styles.checkBox}>
+                <CheckBox
+                  disabled={false}
+                  value={toggleCheckBox}
+                  onValueChange={newValue => setToggleCheckBox(newValue)}
+                  tintColors={{true: 'white', false: 'white'}}
+                />
+                <Text style={{color: '#fff', fontSize: 14}}>Remember me</Text>
+              </View>
+
+              <Text
+                style={{color: '#fff', fontSize: 14}}
+                // onPress={() => {
+                //   props.navigation.navigate('Forgot Password')
+                // }}
+              >
+                I forgot my password
+              </Text>
+            </View>
+                    <Button
+           style={{ width: "80%", marginTop: '10%' }}
+          theme={{ colors: { primary: 'white', onPrimary: "#000" } }}
+          mode="contained"
+         onPress={() => {
+           props.navigation.navigate('Products')
+          }}
+        >
+         Sign In
+        </Button> 
           </View>
-        </View> 
+        </View>
       </SafeAreaView>
     </ScrollView>
-  ); 
+  );
   // return (
   //     <>
   //       <View style={{ ...styles.flex, justifyContent: "space-between", width: "80%" }}>
-  //         <View style={styles.flex} >
-  //           {/* <CheckBox
-  //             disabled={false}
-  //             value={toggleCheckBox}
-  //             onValueChange={(newValue) => setToggleCheckBox(newValue)}
-  //             tintColors={{ true: '#0f0', false: "#fff" }}
-  //           /> */}
-  //           <Text style={{ color: "#fff", fontSize: 16 }}>Remember me</Text>
-  //         </View>
-  //         <Text
-  //           style={{ color: "#fff", fontSize: 14 }}
-  //           // onPress={() => {
-  //           //   props.navigation.navigate('Forgot Password')
-  //           // }}
-  //         >
-  //           I forgot my password
-  //         </Text>
-  //       </View>
   //       {/* <Button
   //         style={{ width: "80%", marginTop: '10%' }}
   //         theme={{ colors: { primary: 'white', onPrimary: "#000" } }}
@@ -108,12 +138,36 @@ const styles = StyleSheet.create({
     padding: 10,
     color: 'black',
   },
-  logo : {
+  logo: {
     alignSelf: 'center',
     marginTop: 100,
   },
   inputContainer: {
     flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    position: 'absolute',
+    top: 29,
+    right: 10,
+    zIndex: 2,
+  },
+  otherOptions: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  checkBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
     alignItems: 'center',
   },
 });
